4306599b291ead0c467b4dcc0cb597d2d858c648
Luke Videckis <lukevideckis@gmail.com>
2023-06-02 06:11:44 -0500

    move to script which calculates and stores expected values

--- calculate_and_store_expected_values.sh
+++ calculate_and_store_expected_values.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 g++ -std=c++17 -O2 calculate_expected_value_table.cpp -o calculate_expected_value_table.out
+test -f expected_value_table.bin || touch expected_value_table.bin
+./calculate_expected_value_table.out
