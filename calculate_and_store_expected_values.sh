0c95942ce7cf75f70cd53f16a803806b27195d3f
Luke Videckis <lukevideckis@gmail.com>
2023-06-02 06:56:25 -0500

    don't re-run when the table already exists

--- calculate_and_store_expected_values.sh
+++ calculate_and_store_expected_values.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 g++ -std=c++17 -O2 calculate_expected_value_table.cpp -o calculate_expected_value_table.out
-test -f expected_value_table.bin || touch expected_value_table.bin
-./calculate_expected_value_table.out
+test -f expected_value_table.bin || touch expected_value_table.bin && ./calculate_expected_value_table.out
